356c1ca894ddeafeeabfb4d3866a29688bff3067
makhfi03 <ghita.makhfi.21@gmail.com>
2026-02-04 14:47:36 +0100

    Déclarer les relations dans Property.php

--- StayEase-app/app/Models/Property.php
+++ StayEase-app/app/Models/Property.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class Property extends Model
 {
     protected $fillable = ['name', 'icon'];
+
+    public function rooms()
+    {
+        return $this->belongsToMany(Room::class, 'rooms_properties');
+    }
 }
