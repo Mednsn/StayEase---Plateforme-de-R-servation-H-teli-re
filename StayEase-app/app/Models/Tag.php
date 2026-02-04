9061593c5c1723c1b13e5524b2cfd34fc49235d0
makhfi03 <ghita.makhfi.21@gmail.com>
2026-02-04 14:47:25 +0100

    Déclarer les relations dans Tag.php

--- StayEase-app/app/Models/Tag.php
+++ StayEase-app/app/Models/Tag.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class Tag extends Model
 {
     protected $fillable = ['name', 'slug'];
+
+    public function rooms()
+    {
+        return $this->belongsToMany(Room::class, 'rooms_tags');
+    }
 }
