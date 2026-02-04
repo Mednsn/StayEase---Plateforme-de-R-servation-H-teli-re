5363038e78a41b057781794292e65734018e6f80
makhfi03 <ghita.makhfi.21@gmail.com>
2026-02-04 14:45:28 +0100

    Déclarer les relations dans Room.php

--- StayEase-app/app/Models/Room.php
+++ StayEase-app/app/Models/Room.php
@@ -7,4 +7,13 @@ use Illuminate\Database\Eloquent\Model;
 class Room extends Model
 {
     protected $fillable = ['hotel_id', 'number', 'price_per_night', 'capacity', 'description'];
+
+    public function tags()
+    {
+        return $this->belongsToMany(Tag::class, 'rooms_tags');
+    }
+    public function properties()
+    {
+        return $this->belongsToMany(Property::class, 'rooms_properties');
+    }
 }
