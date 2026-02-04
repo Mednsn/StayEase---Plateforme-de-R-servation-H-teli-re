1461bbb0add7845840bbe83e30fbed0f1f67f48b
makhfi03 <ghita.makhfi.21@gmail.com>
2026-02-04 14:38:33 +0100

    resolut un petit conflit

--- StayEase-app/routes/web.php
+++ StayEase-app/routes/web.php
@@ -9,16 +9,15 @@ Route::resource('hotels', HotelController::class);
 Route::get('/', function () {
     return view('welcome');
 });
-<<<<<<< HEAD
 
 Route::get('/room', function () {
     return view('create');
-=======
+});
+
 Route::get('/sincription', function () {
     return view('/authentification/regester');
 });
-;
+
 Route::get('/longin', function () {
     return view('/authentification/connection');
->>>>>>> 7312c48937fa773e357afec2ab848103287b04b3
 });
