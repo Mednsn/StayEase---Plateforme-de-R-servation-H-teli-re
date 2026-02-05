a9e1cfb6bba86225aa974e29f921746baf232aa6
makhfi03 <ghita.makhfi.21@gmail.com>
2026-02-05 09:20:16 +0100

    ajouter route de tags

--- StayEase-app/routes/web.php
+++ StayEase-app/routes/web.php
@@ -1,11 +1,12 @@
 <?php
 
+use App\Http\Controllers\TagController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HotelController;
 
 
-Route::get('/',[HotelController::class,'index']);
-Route::resource('hotels', HotelController::class);
+// Route::get('/',[HotelController::class,'index']);
+// Route::resource('hotels', HotelController::class);
 Route::get('/', function () {
     return view('welcome');
 });
@@ -21,3 +22,7 @@ Route::get('/sincription', function () {
 Route::get('/longin', function () {
     return view('/authentification/connection');
 });
+
+Route::resource('tags', TagController::class);
+
+
