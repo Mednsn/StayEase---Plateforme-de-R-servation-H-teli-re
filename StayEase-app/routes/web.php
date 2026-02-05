b2df1d9493be3e76b21854a685a43cd76b07f5b9
mohamed <medolgholb@gmail.com>
2026-02-05 16:18:40 +0100

    j'ai ajouter function qui faire la filtrage par date

--- StayEase-app/routes/web.php
+++ StayEase-app/routes/web.php
@@ -3,9 +3,11 @@
 use App\Http\Controllers\CategoryController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HotelController;
+use App\Http\Controllers\ReservationController;
+use App\Http\Controllers\RoomController;
+use App\Models\Reservation;
 
-
-Route::get('/',[HotelController::class,'index']);
+// Route::get('/',[HotelController::class,'index']);
 Route::resource('hotels', HotelController::class);
 Route::get('/', function () {
     return view('welcome');
@@ -23,3 +25,8 @@ Route::get('/longin', function () {
     return view('/authentification/connection');
 });
 Route::resource('/categories', CategoryController::class); 
+
+Route::get('rooms/check-room', function () {
+    return view('categories.checkRooms');
+});
+Route::post('/rooms/check-room',[ReservationController::class,'index'])->name('room.check-rooms');
