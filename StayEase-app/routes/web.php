ec1d2725bcf36de2ed9df3e00e90737a26be53de
makhfi03 <ghita.makhfi.21@gmail.com>
2026-02-05 12:01:38 +0100

    reglage de conflits

--- StayEase-app/routes/web.php
+++ StayEase-app/routes/web.php
@@ -1,12 +1,9 @@
 <?php
 
-<<<<<<< HEAD
 use App\Http\Controllers\TagController;
 use App\Http\Controllers\PropertyController;
-=======
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\AdminController;
->>>>>>> a6fb4228d736298b2c24e7b8a93eba4abeb6fb52
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HotelController;
 use App\Http\Controllers\UserController;
@@ -15,11 +12,8 @@ use App\Http\Controllers\UserController;
 
 
 
-<<<<<<< HEAD
 // Route::get('/',[HotelController::class,'index']);
 // Route::resource('hotels', HotelController::class);
-=======
->>>>>>> a6fb4228d736298b2c24e7b8a93eba4abeb6fb52
 Route::get('/', function () {
     return view('welcome');
 });
@@ -28,7 +22,7 @@ Route::resource('authentification', UserController::class);
 
 
 Route::resource('hotels', HotelController::class);
-Route::put('/admin/{hotel}/approve', [AdminController::class, 'approve'])->name('admin.approve'); 
+Route::put('/admin/{hotel}/approve', [AdminController::class, 'approve'])->name('admin.approve');
 Route::put('/admin/{hotel}/reject', [AdminController::class, 'reject'])->name('admin.reject');
 Route::resource('admin', adminController::class);
 
@@ -37,7 +31,6 @@ Route::get('/sincription', function () {
     return view('/authentification/regester');
 });
 
-<<<<<<< HEAD
 Route::get('/longin', function () {
     return view('/authentification/connection');
 });
@@ -48,7 +41,5 @@ Route::resource('properties', PropertyController::class);
 
 
 
-=======
 
-Route::resource('/categories', CategoryController::class); 
->>>>>>> a6fb4228d736298b2c24e7b8a93eba4abeb6fb52
+Route::resource('/categories', CategoryController::class);
