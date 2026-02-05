b55d40619cb219f8cfa5a0175f45fe20da924d2a
yassir cherqui <yassirch245@gmail.com>
2026-02-05 15:52:30 +0100

    edit cofler

--- StayEase-app/resources/views/properties/create.blade.php
+++
@@ -1,7 +0,0 @@
-<h1>Ajouter une proprietee</h1>
-<form method="POST" action="{{ route('properties.store') }}">
-    @csrf
-    <label>Name</label>
-    <input type="text" name="name" required>
-    <button>Ajouter</button>
-</form>
