d60009d1abc989e5538b119873778b84262556e2
mouad <mouadzed1@gmail.com>
2026-02-05 15:12:33 +0100

    fix some problem

--- StayEase-app/routes/web.php
+++ StayEase-app/routes/web.php
@@ -9,27 +9,21 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HotelController;
 use App\Http\Controllers\UserController;
 
-<<<<<<< HEAD
 // Route::get('/',[HotelController::class,'index']);
 Route::resource('hotels', HotelController::class);
 
 
-=======
 
 // Route::get('/',[HotelController::class,'index']);
 // Route::resource('hotels', HotelController::class);
->>>>>>> 61a07b2fd87a1178a8e5ab398ea8f9911f157014
 Route::get('/', function () {
     return view('welcome');
 });
 Route::resource('authentification', UserController::class);
-<<<<<<< HEAD
 Route::post('/authentification/login', [UserController::class,'login']);
 
 
 
-=======
->>>>>>> 61a07b2fd87a1178a8e5ab398ea8f9911f157014
 Route::resource('hotels', HotelController::class);
 Route::put('/admin/{hotel}/approve', [AdminController::class, 'approve'])->name('admin.approve');
 Route::put('/admin/{hotel}/reject', [AdminController::class, 'reject'])->name('admin.reject');
@@ -41,12 +35,9 @@ Route::get('/sincription', function () {
 Route::get('/longin', function () {
     return view('/authentification/connection');
 });
-<<<<<<< HEAD
 Route::resource('/categories', CategoryController::class); 
-=======
 
 Route::resource('tags', TagController::class);
 Route::resource('properties', PropertyController::class);
 Route::resource('/categories', CategoryController::class);
 Route::resource('rooms', RoomController::class);
->>>>>>> 61a07b2fd87a1178a8e5ab398ea8f9911f157014
