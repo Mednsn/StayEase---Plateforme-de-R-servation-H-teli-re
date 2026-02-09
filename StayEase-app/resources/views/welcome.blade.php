36581da41298e985044778f52bc47afe7a1136dc
mouad <mouadzed1@gmail.com>
2026-02-09 10:56:53 +0100

    fix the problime  in page welcome

--- StayEase-app/resources/views/welcome.blade.php
+++ StayEase-app/resources/views/welcome.blade.php
@@ -1,19 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
 
-<<<<<<< HEAD
-                                    <a class="btn btn-primary btn-lg px-4 me-sm-3" href=" {{ route('authentification.index') }}">connection</a>
-                                    <a class="btn btn-outline-light btn-lg px-4" href="{{ route('authentification.create') }}">s'inscription</a>
-                                </div>
-                                 @endguest
-                            </div>
-                        </div>  
-                        <div class="col-xl-5 col-xxl-6 d-none d-xl-block text-center"><img class="img-fluid rounded-3 my-5" src="hotel-ar-270120.jpg" alt="..." /></div>
-                    </div>
-                </div>
-            </section>
-            <footer class="bg-primary  py-5 mt-auto">
-=======
 <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
@@ -32,15 +19,24 @@
     <main class="flex-shrink-0">
         <!-- Navigation-->
         <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
->>>>>>> 01d701717c2f75c7cd61f1a8f7ffeb473aabd1f1
             <div class="container px-5">
                 <a class="navbar-brand" href="index.html">StayEase</a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                    data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                    aria-expanded="false" aria-label="Toggle navigation"><span
+                        class="navbar-toggler-icon"></span></button>
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
                         <li class="nav-item"><a class="nav-link" href="/">Home</a></li>
                         <li class="nav-item"><a class="nav-link" href="{{route('hotels.index')}}">Hôtel</a></li>
                         <li class="nav-item"><a class="nav-link" href="contact.html">Contact</a></li>
+                        @auth
+                            <form action="/authentification/logout" method="post">
+                                @csrf
+                                @method('DELETE')
+                                <button class="btn btn-danger btn-lg px-4 me-sm-3">log out</button></li>
+                            </form>
+                        @endauth
                     </ul>
                 </div>
             </div>
@@ -53,13 +49,19 @@
                         <div class="my-5 text-center text-xl-start">
                             <h1 class="display-5 fw-bolder text-white mb-2">The hotel for your weekend or vacation</h1>
                             <p class="lead fw-normal text-white-50 mb-4">The set to search a room for your night </p>
-                            <div class="d-grid gap-3 d-sm-flex justify-content-sm-center justify-content-xl-start">
-                                <a class="btn btn-primary btn-lg px-4 me-sm-3" href=" {{ route('authentification.index') }}">Se connecter</a>
-                                <a class="btn btn-outline-light btn-lg px-4" href="{{ route('authentification.create') }}">S'inscrire</a>
-                            </div>
+                            @guest
+                                <div class="d-grid gap-3 d-sm-flex justify-content-sm-center justify-content-xl-start">
+                                    <a class="btn btn-primary btn-lg px-4 me-sm-3"
+                                        href=" {{ route('authentification.index') }}">Se connecter</a>
+                                    <a class="btn btn-outline-light btn-lg px-4"
+                                        href="{{ route('authentification.create') }}">S'inscrire</a>
+                                </div>
+                            @endguest
                         </div>
                     </div>
-                    <div class="col-xl-5 col-xxl-6 d-none d-xl-block text-center"><img class="img-fluid rounded-3 my-5" src="https://www.kayak.fr/rimg/dimg/dynamic/7/2023/08/dd367b525f8007ab69cc486d19884b31.webp" alt="..." /></div>
+                    <div class="col-xl-5 col-xxl-6 d-none d-xl-block text-center"><img class="img-fluid rounded-3 my-5"
+                            src="https://www.kayak.fr/rimg/dimg/dynamic/7/2023/08/dd367b525f8007ab69cc486d19884b31.webp"
+                            alt="..." /></div>
                 </div>
             </div>
         </header>
